59e98920385ca505d94dca6f1a50673115213d65
nithin <nithinsai2425@>
2026-02-27 12:32:13 +0530

    Initial Commit

---
+++ PashuCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PashuCare
+//
+//  Created by SAIL on 27/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PashuCare/PashuCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PashuCareApp.swift
+//  PashuCare
+//
+//  Created by SAIL on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PashuCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
